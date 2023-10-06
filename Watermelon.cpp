dfe728833e731221e951491dc9603bb97f1479dd
GUNGODD <chauhanadityac01@gmail.com>
2023-10-06 21:55:16 +0530

    WaterMelon

---
+++ Watermelon.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<utility>
+#include<cstdlib>
+using namespace std;
+int main(){
+
+    
+    int n = 0;
+    cin>> n;
+
+    string output = (n <=2 ||  n %2) ? "NO" : "YES";
+    cout << output<< endl;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+
+ // program nn
+
+
+    return 0;
+}
